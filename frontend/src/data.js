4d3549725b145a3e2b56b1bda8db9f5da24ac74e
MalickLayeSy <malicklayesy1@gmail.com>
2021-08-07 20:32:10 +0200

    firt commit

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike sy Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpeg',
+            price:120,
+            brand: 'Nike',
+            rating :4.5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id:'2',
+            name:'Lacost Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p2.jpeg',
+            price:0,
+            brand: 'Nike',
+            rating :4.5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id:'3',
+            name:'Nike sy Shirt',
+            category: 'Shirts',
+            image:'./images/p3.jpeg',
+            price:160,
+            brand: 'Nike',
+            rating :4.5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id:'4',
+            name:'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'./images/p4.jpeg',
+            price:120,
+            brand: 'Nike',
+            rating :4.5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+
+       
+    ],
+};
+
+export default data;
